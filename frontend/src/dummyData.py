8181b00d1da48fd729a7c461916136ed77f4c4af
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-08-12 19:39:53 +0530

    dummy data added

---
+++ frontend/src/dummyData.py
@@ -0,0 +1,60 @@
+
+# Class to info map
+mp = {
+  "Ariel" : {
+    "price" : 40,
+    "ingredients" : "niubibiyb",
+    "nutritional information" : "hbjhbjhbhjb",
+    "manufacturing information" : "jjbiuiub"
+  },
+  "Kurkure" : {
+    "price" : 20,
+    "ingredients" : "niubibiyb",
+    "nutritional information" : "hbjhbjhbhjb",
+    "manufacturing information" : "jjbiuiub"
+  },
+  "Maggie" : {
+    "price" : 30,
+    "ingredients" : "niubibiyb",
+    "nutritional information" : "hbjhbjhbhjb",
+    "manufacturing information" : "jjbiuiub"
+  }
+}
+
+
+# Json returned from model api
+
+{
+  "predictions": [
+    {
+      "x": 337,
+      "y": 302,
+      "width": 390,
+      "height": 454,
+      "confidence": 0.949,
+      "class": "Ariel",
+      "class_id": 0,
+      "detection_id": "bf52eb0c-9aa3-459d-9460-e59ffcc7c3c6"
+    },
+    {
+      "x": 337,
+      "y": 302,
+      "width": 390,
+      "height": 454,
+      "confidence": 0.949,
+      "class": "Phone",
+      "class_id": 0,
+      "detection_id": "bf52eb0c-9aa3-459d-9460-e59ffcc7c3c6"
+    },
+    {
+      "x": 337,
+      "y": 302,
+      "width": 390,
+      "height": 454,
+      "confidence": 0.949,
+      "class": "Kurkure",
+      "class_id": 0,
+      "detection_id": "bf52eb0c-9aa3-459d-9460-e59ffcc7c3c6"
+    }
+  ]
+}
