1642d40de9c0eed4f9af9ffbba2bf5a61cc9325f
Divyansh Singh <divyanshracvik2209@gmail.com>
2024-08-14 18:17:36 +0530

    admin dashboard done

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,105 +1,32 @@
-const people = [
-    {
-        name: 'Leslie Alexander',
-        email: 'leslie.alexander@example.com',
-        role: 'Co-Founder / CEO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Michael Foster',
-        email: 'michael.foster@example.com',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Dries Vincent',
-        email: 'dries.vincent@example.com',
-        role: 'Business Relations',
-        imageUrl:
-            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-    {
-        name: 'Lindsay Walton',
-        email: 'lindsay.walton@example.com',
-        role: 'Front-end Developer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Courtney Henry',
-        email: 'courtney.henry@example.com',
-        role: 'Designer',
-        imageUrl:
-            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: '3h ago',
-        lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        role: 'Director of Product',
-        imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        lastSeen: null,
-    },
-]
+import React from 'react'
+import AdminOrders from '../components/ui/AdminOrders'
+import AdminItems from '../components/ui/AdminItems'
 
 type Props = {}
 
 const Admin = (props: Props) => {
     return (
-        <div className="flex justify-center items-center flex-col">
-            <div className="text-3xl font-bold text-white m-7">Item list</div>
-            <div className="w-2/3	">
-                <ul role="list" className="divide-y divide-gray-100 border p-3 rounded-2xl">
-                    {people.map((person) => (
-                        <li key={person.email} className="flex justify-between gap-x-6 py-5">
-                            <div className="flex min-w-0 gap-x-4">
-                                <img alt="" src={person.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
-                                <div className="min-w-0 flex-auto">
-                                    <p className="text-lg text-white  font-semibold leading-6">{person.name}</p>
-                                    <p className="mt-1 truncate text-md leading-5 text-gray-300">{person.email}</p>
-                                </div>
-                            </div>
-                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-                                {person.lastSeen ? (
-                                    <p className="mt-1 text-md leading-5 text-gray-400">
-                                        Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                                    </p>
-                                ) : (
-                                    <div className="mt-1 flex items-center gap-x-1.5">
-                                        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                                        </div>
-                                        <p className="text-md leading-5 text-gray-400">Online</p>
-                                    </div>
-                                )}
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            <div className="m-5">
-                <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                    Add Items
-                </button>
+        <div className='bg-[#151b25] h-full '>
+            <div className='py-6 flex justify-center items-center flex-col border-b-2'>
+                <p className='text-white text-6xl font-extrabold'>Admin Dashboard</p>
             </div>
 
 
+            <div className='py-6 flex justify-center items-center flex-col'>
+                <p className='text-white text-5xl font-extrabold'>Orders</p>
+            </div>
+            <div>
+                <AdminOrders />
+            </div>
+
+            <div className='py-6 flex justify-center items-center flex-col'>
+                <p className='text-white text-5xl font-extrabold'>Inventory</p>
+            </div>
+            <div>
+                <AdminItems />
+            </div>
         </div>
     )
 }
 
 export default Admin
-
