e9925f993e3d3e28fe11ca472c298eb1d6968519
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-08-16 14:49:36 +0530

    camera switching

--- frontend/src/pages/TalkToYourProduct.tsx
+++ frontend/src/pages/TalkToYourProduct.tsx
@@ -84,7 +84,7 @@ const TalkToYourProduct = () => {
   useEffect(() => {
     const openCameraAndTakePicture = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
           videoRef.current.play();
